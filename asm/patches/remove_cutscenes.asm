8f9524f4b935c1abb38c5d4b2307a5d42a0d1c46
Crain-32 <25422785+Crain-32@users.noreply.github.com>
2024-09-17 18:17:50 -0600

    Removed the removal of the intro cutscene

--- asm/patches/remove_cutscenes.asm
+++ asm/patches/remove_cutscenes.asm
@@ -3,12 +3,12 @@
 
 
 ; nop out a couple lines so the long intro movie is skipped.
-.open "sys/main.dol"
-.org 0x80232C78
-  nop
-.org 0x80232C88
-  nop
-.close
+;.open "sys/main.dol"
+;.org 0x80232C78
+;  nop
+;.org 0x80232C88
+;  nop
+;.close
 
 
 
